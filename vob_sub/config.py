92962e324b5e79481dba9d6155faf59ea96e747c
vincrichard <55540832+vincrichard@users.noreply.github.com>
2025-02-12 14:34:30 +0900

    [Fix] use of field for the default general in setting args

--- vob_sub/config.py
+++ vob_sub/config.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from dataclasses import field
 
 # Default settings found in SubTitle
 @dataclass
@@ -8,4 +9,4 @@ class SettingsArgs:
         minimum_milliseconds_between_lines: int = 24
         subtitle_maximum_display_milliseconds: int = 8 * 1000
 
-    general: GeneralArgs = GeneralArgs()
+    general: GeneralArgs = field(default_factory=GeneralArgs)
